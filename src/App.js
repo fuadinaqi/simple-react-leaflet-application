121b17e422a15004e2640196237b1e064d7c7a32
fuadinaqi <fuadinaqi@aisifoundation.org>
2018-04-06 09:05:50 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import HeaderTitle from './components/HeaderTitle.jsx'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HeaderTitle/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/HeaderTitle.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../App.css'
+
+export default class HeaderTitle extends Component {
+  render() {
+    return (
+      <div style={{marginTop: 70}}>
+        <h1 className="title">Welcome to MAP</h1>
+      </div>
+    )
+  }
+};
